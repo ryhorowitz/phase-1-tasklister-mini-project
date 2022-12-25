53edf66e8c1f5a6f63fccf2543e7753118d1a337
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-12-25 14:31:26 -0500

    Completed Bare Minimum

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const input = document.getElementById("new-task-description")
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    createListItem(input.value)
+  })
+
 });
+
+function createListItem(val) {
+  console.log('val is, ', val)
+  let item = document.createElement('li')
+  let list = document.getElementById('tasks')
+  item.textContent = val
+  list.appendChild(item)
+}
